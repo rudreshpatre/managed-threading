332660404d5042ac2750019862eb193175520f04
Rudresh Patre <rudreshpatre@Rudreshs-MacBook-Pro.local>
2023-02-23 21:41:15 +0530

    Add comments, describing the about foreground and background threads

--- BackgroundThread/BackgroundThread/Program.cs
+++ BackgroundThread/BackgroundThread/Program.cs
@@ -1,6 +1,20 @@
 ﻿namespace BackgroundThread;
 class Program
 {
+    // Puprose of snippet: Understand Foreground and Background threads
+
+    // In this snippet we are creating a new thread, apart from the
+    // default main thread on which .net excutes the code.
+    // The newly created thread can be set as background thread by setting the
+    // Thread.IsBackground property as true.
+    // If we dont set it as true, be default Foreground thread is created.
+    // What is the difference?
+    // We can terminate a process or app or exe even if background thread is executing
+    // But its not the same for foreground thread, in order to exit the app we willl have to
+    // stop the debugger or close the command window.
+    // For a background thread, we the framework gives lower priority and hence we allows to termintate the
+    // main process.
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
@@ -17,7 +31,7 @@ class Program
             }
         });
 
-        bgThread.IsBackground = false;
+        bgThread.IsBackground = true;
         bgThread.Start();
 
         for (int i = 0; i < 10; i++)
